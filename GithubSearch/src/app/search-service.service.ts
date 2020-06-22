e00be6330c540671314d8a5e3b1330d13e385ec4
BTG001 <brunokipkorir2@gmail.com>
2020-06-22 15:53:12 +0300

    Add Repository and User blueprint class to the search-service and defined them in the search-service class

--- GithubSearch/src/app/search-service.service.ts
+++ GithubSearch/src/app/search-service.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Repository } from './repository';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchServiceService {
+  repository: Repository;
+    users: User;
+    newRepository: any;
+    searchRepo: any;
 
   constructor() { }
 }
