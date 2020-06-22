fee99b33705e3ec08d256a8516e8c83a701a94ee
BTG001 <brunokipkorir2@gmail.com>
2020-06-22 20:30:49 +0300

    import the loader that listens for our HTTP requests from the app and automatically displays progress according to the apps requests

--- GithubSearch/src/app/app.module.ts
+++ GithubSearch/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { SearchServiceService } from './search-service.service';
 import { HttpClientModule } from '@angular/common/http';
 import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
 
 
 
@@ -30,6 +31,7 @@ import { SearchFormComponent } from './search-form/search-form.component';
     FormsModule,
     HttpClientModule,
     NgProgressModule.forRoot(),
+    NgProgressHttpClientModule
   ],
   providers: [SearchServiceService],
   bootstrap: [AppComponent]
