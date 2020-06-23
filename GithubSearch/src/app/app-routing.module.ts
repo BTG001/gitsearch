b84a820facc8a3f8c662117f53e429787ab6a4c5
BTG001 <brunokipkorir2@gmail.com>
2020-06-23 15:12:32 +0300

    Create an empty path with the redirectTo property that points to the path of the UserDetailsComponent. The router will only redirect to the /users path if the URL given is empty ' '.

--- GithubSearch/src/app/app-routing.module.ts
+++ GithubSearch/src/app/app-routing.module.ts
@@ -13,6 +13,8 @@ const routes: Routes = [
   { path:'**', component: NotFoundComponent},
   {path: '',redirectTo: '/users', pathMatch: 'full'},
 
+  { path: '', redirectTo:"/users", pathMatch:"full"},
+
 ];
 
 @NgModule({
